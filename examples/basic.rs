01a9e3a629f72c9203ba1404b238b44bac069058
Corresation <214865185+Corresation@users.noreply.github.com>
2026-07-28 23:51:10 +0100

    throttle controls validated

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,24 +1,41 @@
-use rocketsim_rs::sim::{Arena, CarConfig, Team};
+use rocketsim_rs::sim::{Arena, CarConfig, CarControls, Team};
 
 fn main() {
     rocketsim_rs::init(None, true);
 
-    let mut arena = Arena::default_standard();
+    for throttle in [1.0, 0.0, -1.0] {
+        let mut arena = Arena::default_standard();
 
-    println!("tick rate: {}", arena.get_tick_rate());
+        println!("tick rate: {}", arena.get_tick_rate());
 
-    let car_id = arena.pin_mut().add_car(Team::Blue, CarConfig::octane());
-    let before = arena.pin_mut().get_car(car_id);
+        let car_id = arena.pin_mut().add_car(Team::Blue, CarConfig::octane());
+        let before = arena.pin_mut().get_car(car_id);
 
-    let ticks = 120;
+        arena
+            .pin_mut()
+            .set_car_controls(
+                car_id,
+                CarControls {
+                    throttle,
+                    ..Default::default()
+                },
+            )
+            .unwrap();
 
-    arena.pin_mut().step(ticks);
+        let ticks = 60;
 
-    println!("stepped {ticks} ticks");
+        arena.pin_mut().step(ticks);
 
-    let after = arena.pin_mut().get_car(car_id);
+        println!("stepped {ticks} ticks");
 
-    println!("car id: {car_id}");
-    println!("before: {}", before.pos);
-    println!("after: {}", after.pos);
+        let after = arena.pin_mut().get_car(car_id);
+
+        println!("throttle: {throttle}");
+        println!("car id: {car_id}");
+        println!("before position: {}", before.pos);
+        println!("before velocity: {}", before.vel);
+        println!("after position: {}", after.pos);
+        println!("after velocity: {}", after.vel);
+        println!();
+    }
 }
