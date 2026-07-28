02a483e5a930aad6e786080ba15660bb74de0268
Corresation <214865185+Corresation@users.noreply.github.com>
2026-07-28 23:03:18 +0100

    add car state example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,10 +1,24 @@
-use rocketsim_rs::sim::Arena;
+use rocketsim_rs::sim::{Arena, CarConfig, Team};
 
 fn main() {
     rocketsim_rs::init(None, true);
 
     let mut arena = Arena::default_standard();
-    arena.pin_mut().step(1);
 
     println!("tick rate: {}", arena.get_tick_rate());
+
+    let car_id = arena.pin_mut().add_car(Team::Blue, CarConfig::octane());
+    let before = arena.pin_mut().get_car(car_id);
+
+    let ticks = 120;
+
+    arena.pin_mut().step(ticks);
+
+    println!("stepped {ticks} ticks");
+
+    let after = arena.pin_mut().get_car(car_id);
+
+    println!("car id: {car_id}");
+    println!("before: {}", before.pos);
+    println!("after: {}", after.pos);
 }
